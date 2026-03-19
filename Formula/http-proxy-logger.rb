2c90e7006bb94254801cf735e49e3c8d9708ebe2
Stanislav Deviatov <devyatov@gmail.com>
2026-03-19 18:28:15 +0100

    chore: update http-proxy-logger to 1.2.3

--- Formula/http-proxy-logger.rb
+++ Formula/http-proxy-logger.rb
@@ -4,19 +4,19 @@
 class HttpProxyLogger < Formula
   desc "HTTP reverse-proxy with colored request/response logging"
   homepage "https://github.com/stn1slv/http-proxy-logger"
-  version "1.2.2"
+  version "1.2.3"
   license "MIT"
 
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/stn1slv/http-proxy-logger/releases/download/v.1.2.2/http-proxy-logger_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/stn1slv/http-proxy-logger/releases/download/v.1.2.3/http-proxy-logger_darwin_amd64.tar.gz"
+      sha256 "15f52f3918c480513f92bd8c36d4c4f9ea5bd0bad59d7482f3bb5eac84c5ce96"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/stn1slv/http-proxy-logger/releases/download/v.1.2.2/http-proxy-logger_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/stn1slv/http-proxy-logger/releases/download/v.1.2.3/http-proxy-logger_darwin_arm64.tar.gz"
+      sha256 "8796dc1f0801ac35211f143bb0df88036c966212f7431046982a2ed70475408f"
     end
   end
 
